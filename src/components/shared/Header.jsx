fa20bcb70ba2391ea8a904398a215d01e5821405
ferdouszihad <f.zihad333@gmail.com>
2025-03-26 00:11:57 +0600

    context integrated by using use Hook

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,7 +1,14 @@
+import { use } from "react";
+import { ThemeContext } from "../../context/ThemeProvider";
+
 const Header = () => {
+  const { theme, toggleTheme } = use(ThemeContext);
   return (
-    <div className="navbar bg-primary text-primary-content shadow">
+    <div className="navbar bg-primary justify-between  text-primary-content shadow">
       <button className="btn btn-secondary text-xl">Explore REACT-19</button>
+      <button onClick={toggleTheme} className="btn btn-secondary text-xl">
+        {theme == "light" ? "dark" : "light"}
+      </button>
     </div>
   );
 };
---
+++ src/context/ThemeProvider.jsx
@@ -0,0 +1,22 @@
+import { createContext, useState } from "react";
+
+export const ThemeContext = createContext();
+const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+
+  const toggleTheme = () => {
+    setTheme((theme) => (theme == "light" ? "dark" : "light"));
+    localStorage.setItem("theme", theme == "light" ? "dark" : "light");
+  };
+  const themeFeatures = {
+    theme,
+    toggleTheme,
+  };
+  return (
+    <div data-theme={theme} className="duration-1000">
+      <ThemeContext value={themeFeatures}>{children}</ThemeContext>
+    </div>
+  );
+};
+
+export default ThemeProvider;
